933dbb74f6ea54cf85633fde3e7ef0fc11a4e26c
Madhuri Malviya <madhurimalviya2601@gmail.com>
2025-07-30 10:49:10 +0530

    removed token_format parameter

--- Modules/gcs/config/main.tf
+++ Modules/gcs/config/main.tf
@@ -13,7 +13,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "default" {
-  name     = "gcs-bucket-github-demo1"
+  name     = "gcs-bucket-github-demo"
   location = "us-central1"
   force_destroy = true
 }
