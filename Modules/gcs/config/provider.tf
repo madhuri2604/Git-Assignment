0f7312f49b34c012ff21fb536de4c4ebafff92da
Madhuri Malviya <madhurimalviya2601@gmail.com>
2025-07-31 15:36:00 +0530

    test imperonation sa

--- Modules/gcs/config/provider.tf
+++ Modules/gcs/config/provider.tf
@@ -1,4 +1,4 @@
 provider "google" {
-  impersonate_service_account = "sa-workload-identity-fed@prj-gcp-inframod-01.iam.gserviceaccount.com"
+  impersonate_service_account = "ado-test-sa@prj-gcp-inframod-01.iam.gserviceaccount.com"
   project                     = "prj-gcp-inframod-01"
 } 
