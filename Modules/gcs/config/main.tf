b573fceadb32751d6ccd2a0dcb6ab59a4f2bc7ee
Madhuri Malviya <madhurimalviya2601@gmail.com>
2025-08-01 19:03:16 +0530

    test 2

--- Modules/gcs/config/main.tf
+++ Modules/gcs/config/main.tf
@@ -7,16 +7,10 @@ terraform {
   }
 }
 
-# resource "google_storage_bucket" "default" {
-#   name     = var.bucket_name
-#   location = "us-central1"
-#   force_destroy = true
-#   uniform_bucket_level_access = true
-#   project                     = "prj-gcp-inframod-02"
-# }
-
-resource "google_folder" "folders" {
-  display_name        = "wif-test-folder"
-  parent              = "folders/815968443088"
-#   deletion_protection = false
+resource "google_storage_bucket" "default" {
+  name     = var.bucket_name
+  location = "us-central1"
+  force_destroy = true
+  uniform_bucket_level_access = true
+  project                     = "prj-gcp-inframod-02"
 }
