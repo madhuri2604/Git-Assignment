fbdc1ecd673d7241663d15a2bea0750f8448b00b
Madhuri Malviya <madhurimalviya2601@gmail.com>
2025-08-01 18:07:58 +0530

    deploy folder test

--- Modules/gcs/config/main.tf
+++ Modules/gcs/config/main.tf
@@ -7,10 +7,16 @@ terraform {
   }
 }
 
-resource "google_storage_bucket" "default" {
-  name     = var.bucket_name
-  location = "us-central1"
-  force_destroy = true
-  uniform_bucket_level_access = true
-  project                     = "prj-gcp-inframod-02"
+# resource "google_storage_bucket" "default" {
+#   name     = var.bucket_name
+#   location = "us-central1"
+#   force_destroy = true
+#   uniform_bucket_level_access = true
+#   project                     = "prj-gcp-inframod-02"
+# }
+
+resource "google_folder" "folders" {
+  display_name        = "wif-test-folder"
+  parent              = "folders/815968443088"
+  deletion_protection = false
 }
