6e07220d7fb77a80208e26277155cc3fa4025679
Madhuri Malviya <madhurimalviya2601@gmail.com>
2025-08-04 01:03:19 +0530

    test provider as sa token

--- Modules/gcs/config/provider.tf
+++ Modules/gcs/config/provider.tf
@@ -1,4 +1,10 @@
+# provider "google" {
+#   impersonate_service_account = "ado-test-sa@prj-gcp-inframod-01.iam.gserviceaccount.com"
+#   project                     = "prj-gcp-inframod-01"
+# } 
+
 provider "google" {
-  impersonate_service_account = "ado-test-sa@prj-gcp-inframod-01.iam.gserviceaccount.com"
+  credentials = file("/home/runner/adc.json")
   project                     = "prj-gcp-inframod-01"
-} 
+
+}
