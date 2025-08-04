9cc8f70a52053367681b986ca4ebd3f5e10808c4
Madhuri Malviya <madhurimalviya2601@gmail.com>
2025-08-04 11:33:36 +0530

    setting token fortmat as access tokens

--- Modules/gcs/config/provider.tf
+++ Modules/gcs/config/provider.tf
@@ -1,10 +1,5 @@
-# provider "google" {
-#   impersonate_service_account = "ado-test-sa@prj-gcp-inframod-01.iam.gserviceaccount.com"
-#   project                     = "prj-gcp-inframod-01"
-# } 
-
 provider "google" {
-  credentials = file("/home/runner/adc.json")
+  impersonate_service_account = "ado-test-sa@prj-gcp-inframod-01.iam.gserviceaccount.com"
   project                     = "prj-gcp-inframod-01"
+} 
 
-}
