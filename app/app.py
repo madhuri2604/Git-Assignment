080f4c27206c98891f377984fbc6a7cbcda44a04
Madhuri Malviya <madhurimalviya2601@gmail.com>
2025-08-21 12:12:03 +0530

    provide feature branch

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ cors = CORS(app, resources={r"*": {"origins": "*"}})
 def index():
     random_string = ''.join(random.choice(string.ascii_letters) for i in range(10))
     return "v1-" + random_string
-
+ 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
     
